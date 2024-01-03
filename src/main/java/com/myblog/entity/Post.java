0bea8fd4c3b8a414381f1bd8f04d331d3ea54436
shahrukh khan <hiiisrk01@gmail.com>
2024-01-03 21:28:47 +0530

    127: create save post feature Entity class is created

---
+++ src/main/java/com/myblog/entity/Post.java
@@ -0,0 +1,30 @@
+package com.myblog.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Table(name="posts")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Post {
+
+
+     @Id
+     @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private long id;
+
+      private String title;
+
+    private String description;
+
+    private  String content;
+
+
+
+
+}
